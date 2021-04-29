aaf53c6ae2e9ffd3f2fc7c00b865d04cbeb08ad3
trivergod123 <82467786+trivergod123@users.noreply.github.com>
2021-04-29 13:56:08 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
+namespace SpriteKind {
+    export const Pokeball = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    Pokeball.vy += -100
+    Pokeball.vy += -15
+})
+sprites.onCreated(SpriteKind.Pokeball, function (sprite) {
+    sprite.setPosition(randint(10, 140), randint(10, 140))
+})
+sprites.onOverlap(SpriteKind.Pokeball, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.setPosition(randint(10, 140), randint(10, 140))
+    music.baDing.play()
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    Pokeball.vx += -100
+    Pokeball.vx += -15
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    Pokeball.vx += 100
+    Pokeball.vx += 15
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    Pokeball.vy += 100
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
-    game.over(true)
+    Pokeball.vy += 15
 })
 let Pokeball: Sprite = null
 scene.setBackgroundImage(img`
@@ -136,41 +144,93 @@ scene.setBackgroundImage(img`
     ................................................................................................................................................................
     ................................................................................................................................................................
     `)
-Pokeball = sprites.create(img`
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 b c 5 5 d 4 c . . 
-    . b b b b 5 5 5 b f d d 4 4 4 b 
-    . b d 5 b 5 5 b c b 4 4 4 4 b . 
-    . . b 5 5 b 5 5 5 4 4 4 4 b . . 
-    . . b d 5 5 b 5 5 5 5 5 5 b . . 
-    . b d b 5 5 5 d 5 5 5 5 5 5 b . 
-    b d d c d 5 5 b 5 5 5 5 5 5 b . 
-    c d d d c c b 5 5 5 5 5 5 5 b . 
-    c b d d d d d 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
-    `, SpriteKind.Player)
+Pokeball = sprites.create(assets.image`pokeball`, SpriteKind.Player)
 Pokeball.setPosition(10, 54)
 Pokeball.setStayInScreen(true)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+let Pokemon1 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Player)
+Pokemon1.setPosition(randint(10, 100), randint(10, 100))
+Pokemon1.setStayInScreen(true)
+let Pokemon2 = sprites.create(img`
+    . . . . . . . . . . . . . . 
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e . b f b 
+    f d d f d d f d d f . f d f 
+    f b d d b b d d 2 b f f d f 
+    . f 2 2 2 2 2 2 d b b d b f 
+    . f d d d d d d d f f f f . 
+    . . f d b d f d f . . . . . 
+    . . . f f f f f f . . . . . 
+    `, SpriteKind.Player)
+Pokemon2.setPosition(randint(10, 100), randint(10, 100))
+Pokemon2.setStayInScreen(true)
+let Pokemon3 = sprites.create(img`
+    . . . . . . . . c c c c c . . . 
+    . . . . . . c c 5 5 5 5 5 c . . 
+    . . . . . c 5 5 5 5 5 5 5 5 c . 
+    . . . . c b b b b b b 5 5 5 c . 
+    . . . . c 1 1 b b 1 b b c c . . 
+    . . . c 1 1 1 b b 1 1 1 c . . . 
+    . . . c 1 1 1 1 b 1 1 1 c . c c 
+    . . . c d 1 1 1 b 1 1 1 b b 5 c 
+    . . c c d 1 c 1 b 1 b 1 5 5 5 c 
+    . c c d d 1 1 1 1 1 b 1 b b 5 c 
+    f d d d 1 1 1 1 1 b b 1 f . c c 
+    f f f 1 1 1 1 1 1 b b b f . . . 
+    . . . f f 1 1 1 b b b 5 5 f . . 
+    . . . . . f f f 5 5 5 5 5 f . . 
+    . . . . . . . . f f f f f f . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+Pokemon3.setPosition(randint(0, 150), randint(10, 150))
+Pokemon3.setStayInScreen(true)
+let Pokemon4 = sprites.create(img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c b b b b b b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b c b b b c b b b b f . . . . 
+    f b 1 f f f 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, SpriteKind.Player)
+Pokemon4.setPosition(randint(10, 140), randint(10, 140))
+Pokemon4.setStayInScreen(true)
+info.setScore(0)
